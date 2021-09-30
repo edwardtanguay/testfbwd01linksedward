82e88b64174b5fb8810df0a4fdf6decbe59297fd
Edward Tanguay (hm.win) <edward@tanguay.info>
2021-09-30 06:34:45 +0200

    before push

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.scss';
+import links from './data/links.json';
 
 function App() {
   return (
     <div className="App">
-		Hello
+		<h1>Links</h1>
+		  <ul>
+			  {links.map((link, index) => {
+				  return (
+					  <li key={index}><a target="_blank" href={link.url} rel="noreferrer">{link.title}</a></li>
+				  )
+			  })}
+		</ul>
     </div>
   );
 }
