d3d648169885d612b875248619b60756b810c76b
Edward Tanguay (hm.win) <edward@tanguay.info>
2021-09-30 06:55:59 +0200

    changed to fetch

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
+import React, { useState , useEffect} from 'react'
 import './App.scss';
-import links from './data/links.json';
 
 function App() {
-  return (
-    <div className="App">
-		<h1>Links</h1>
-		  <ul>
-			  {links.map((link, index) => {
-				  return (
-					  <li key={index}><a target="_blank" href={link.url} rel="noreferrer">{link.title}</a></li>
-				  )
-			  })}
-		</ul>
-    </div>
-  );
+
+	const [links, setLinks] = useState([]);
+
+	useEffect(() => {
+		(async () => {
+			const response = await fetch('https://raw.githubusercontent.com/edwardtanguay/testfbwd01linksedward/main/src/data/links.json');
+			const data = await response.json();
+			console.log(data);
+			setLinks(data);
+		})();
+	}, [])
+
+	return (
+		<div className="App">
+			<h1>Links</h1>
+			<ul>
+				{links.map((link, index) => {
+					return (
+						<li key={index}><a target="_blank" href={link.url} rel="noreferrer">{link.title}</a></li>
+					)
+				})}
+			</ul>
+		</div>
+	);
 }
 
 export default App;
